63e037d9ef2cdb1bd8d1d5650e8377e159a11f9f
Catalyn45 <ancutei.catalin@gmail.com>
2022-01-11 22:49:07 +0200

    added zsh plugins

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -4,5 +4,7 @@ sudo pacman -S zsh # or `sudo apt install zsh` on ubuntu
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 cp ./.zshrc ~/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 zsh
 
