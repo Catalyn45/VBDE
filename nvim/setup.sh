d601da6502eab51c6c16e492d686bd681e45671c
cancutei <cancutei@bitdefender.com>
2022-02-01 20:08:08 +0200

    -

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -6,20 +6,18 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 mkdir -p ~/.config/nvim/plugged
-cd ~/.config/nvim/plugged
 
-git clone https://github.com/kien/ctrlp.vim.git
-git clone https://github.com/nikvdp/ejs-syntax.git
-git clone https://github.com/morhetz/gruvbox.git
-git clone https://github.com/preservim/nerdtree.git
-git clone https://github.com/vim-airline/vim-airline.git
-git clone https://github.com/vim-airline/vim-airline-themes.git
-git clone https://github.com/rhysd/vim-clang-format.git
-git clone https://github.com/tpope/vim-fugitive.git
-git clone https://github.com/airblade/vim-gitgutter.git
-git clone https://github.com/vim-utils/vim-man.git
-
-cd -
+sh -c 'cd ~/.config/nvim/plugged && \
+git clone https://github.com/kien/ctrlp.vim.git && \
+git clone https://github.com/nikvdp/ejs-syntax.git && \
+git clone https://github.com/morhetz/gruvbox.git && \
+git clone https://github.com/preservim/nerdtree.git && \
+git clone https://github.com/vim-airline/vim-airline.git && \
+git clone https://github.com/vim-airline/vim-airline-themes.git && \
+git clone https://github.com/rhysd/vim-clang-format.git && \
+git clone https://github.com/tpope/vim-fugitive.git && \
+git clone https://github.com/airblade/vim-gitgutter.git && \
+git clone https://github.com/vim-utils/vim-man.git'
 
 cp ./init.vim ~/.config/nvim/
 nvim -c "PlugInstall | qall"
