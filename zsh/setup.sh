c390743f1afcaebdecdbdcb325e00a2284d6688b
Catalyn45 <ancutei.catalin@gmail.com>
2022-05-18 23:29:10 +0300

    fixes

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -7,5 +7,7 @@ git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerl
 cp ./zsh/.zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-zsh
+
+source ~/.zshrc
+p10k configure
 
