80894b40e6eea7000015b090c0725b8a3ab8ad59
catalin iulian <ancutei.catalin@gmail.com>
2022-08-11 01:03:00 +0300

    Another fix

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -4,10 +4,14 @@ export RUNZSH=no
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 source ~/.zshrc
-
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+rm ~/.zshrc
 ln -s $PWD/.zshrc ~/
+
+rm ~/.p10k.zsh
 ln -s $PWD/.p10k.zsh ~/
+
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
