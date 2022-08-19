1a871db50a8de6f4a61de65fc4de75925dce2fed
Catalyn45 <ancutei.catalin@gmail.com>
2022-08-19 13:04:18 +0300

    Changed zsh setup orded

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -3,13 +3,14 @@
 rm ~/.zshrc
 ln -s $PWD/.zshrc ~/
 
-rm ~/.p10k.zsh
-ln -s $PWD/.p10k.zsh ~/
-
 export RUNZSH=no
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+rm ~/.p10k.zsh
+ln -s $PWD/.p10k.zsh ~/
+
 source ~/.zshrc
+
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
