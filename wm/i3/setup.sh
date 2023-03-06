5e87d921ff83e0b8f344e00d749dcb714731c815
catalin iulian <ancutei.catalin@gmail.com>
2023-03-06 19:57:00 +0200

    updated i3 setup

--- wm/i3/setup.sh
+++ wm/i3/setup.sh
@@ -5,3 +5,8 @@ install i3-gaps
 install i3lock
 
 replace_config ~/.config/i3 config
+replace_config ~/.config/i3 binds.conf
+replace_config ~/.config/i3 monitors.conf
+replace_config ~/.config/i3 startups.conf
+replace_config ~/.config/i3 workspaces.conf
+
