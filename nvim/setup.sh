cddd5a08860f97342a6580a8ebeff1a5a50b441a
catalin iulian <ancutei.catalin@gmail.com>
2023-04-24 18:00:02 +0300

    fixed neovim installer

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -15,12 +15,13 @@ replace_config ~/.config/nvim init.lua
 
 mkdir -p ~/.config/nvim/lua
 
-replace_config ~/.config/nvim/lua plugins.lua
-replace_config ~/.config/nvim/lua shortcuts.lua
-replace_config ~/.config/nvim/lua utils.lua
-replace_config ~/.config/nvim/lua lsp.lua
-replace_config ~/.config/nvim/lua parser.lua
-replace_config ~/.config/nvim/lua debugger.lua
+replace_config ~/.config/nvim/lua lua/plugins.lua
+replace_config ~/.config/nvim/lua lua/shortcuts.lua
+replace_config ~/.config/nvim/lua lua/utils.lua
+replace_config ~/.config/nvim/lua lua/lsp.lua
+replace_config ~/.config/nvim/lua lua/parser.lua
+replace_config ~/.config/nvim/lua lua/debugging.lua
 
 nvim -c "PlugInstall | qall" 2> /dev/null
 
+replace_config ~/.config/nvim/plugged/switcher a.vim
